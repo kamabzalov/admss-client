0c46398a59bf25f701120557c9f25bbce970f383
Kamil Abzalov <mail@kamil-abzalov.com>
2023-08-01 23:02:02 +0300

    Add form submit api

--- src/sign/sign-in/index.tsx
+++ src/sign/sign-in/index.tsx
@@ -1,8 +1,8 @@
-import { SubmitHandler } from "react-hook-form";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Checkbox } from "primereact/checkbox";
 import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 
 interface LoginForm {
   username: string;
@@ -12,27 +12,49 @@ interface LoginForm {
 
 export default function SignIn() {
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    validate: (data: { username: string, password: string }) => {
+      let errors: any = {};
 
-  const login: SubmitHandler<LoginForm> = data => {
-    navigate("/dashboard");
-  };
+      if (!data.username.trim()) {
+        errors.username = "Username is required.";
+      }
+
+      if (!data.password.trim()) {
+        errors.password = "Password is required.";
+      }
+
+      return errors;
+    },
+    onSubmit: () => {
+      navigate('/dashboard');
+    },
+  });
 
   return (
     <section className="sign">
       <div className="sign-in">
         <h1 className="sign__title">Sign In</h1>
-        <form noValidate>
+        <form onSubmit={formik.handleSubmit}>
           <div className="m-b-15">
             <div className="p-input-icon-right w-full">
               <i className="admss-icon-username sign__icon" />
-              <InputText className="sign__input" id="username" />
+              <InputText className="sign__input" id="username" onChange={formik.handleChange}
+                         value={formik.values.username} />
+              {formik.errors.username ? <div>{formik.errors.username}</div> : null}
             </div>
           </div>
 
           <div className="m-b-10">
             <div className="p-input-icon-right w-full">
               <i className="admss-icon-password sign__icon" />
-              <InputText className="sign__input" id="password" type="password" />
+              <InputText className="sign__input" id="password" type="password" onChange={formik.handleChange}
+                         value={formik.values.password} />
+              {formik.errors.password ? <div>{formik.errors.password}</div> : null}
             </div>
           </div>
 
@@ -48,7 +70,7 @@ export default function SignIn() {
             </Link>
           </div>
           <p className="text-center">
-            <Button label="Sign in" severity="success" className="sign__button" />
+            <Button label="Sign in" severity="success" type="submit" className="sign__button" />
           </p>
         </form>
       </div>
