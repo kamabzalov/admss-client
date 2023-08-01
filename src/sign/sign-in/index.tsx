0070c955be22817553b69aca33d809bdc1193ea6
Kamil Abzalov <mail@kamil-abzalov.com>
2023-08-01 22:38:32 +0300

    Redesign sign in form

--- src/sign/sign-in/index.tsx
+++ src/sign/sign-in/index.tsx
@@ -1,8 +1,8 @@
+import { SubmitHandler } from "react-hook-form";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Checkbox } from "primereact/checkbox";
 import { Link, useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
 
 interface LoginForm {
   username: string;
@@ -12,70 +12,45 @@ interface LoginForm {
 
 export default function SignIn() {
   const navigate = useNavigate();
-  const formik = useFormik<LoginForm>({
-    initialValues: {
-      username: "",
-      password: "",
-      rememberMe: false
-    },
-    validate: (data: { username: string, password: string }) => {
-      let errors: any = {};
 
-      if (!data.username.trim()) {
-        errors.username = "Username is required.";
-      }
-
-      if (!data.password.trim()) {
-        errors.password = "Password is required.";
-      }
-
-      return errors;
-    },
-    onSubmit: () => {
-      navigate('/dashboard');
-    },
-  });
+  const login: SubmitHandler<LoginForm> = data => {
+    navigate("/dashboard");
+  };
 
   return (
     <section className="sign">
       <div className="sign-in">
-        <div className='sign-in-wrapper'>
-          <h1 className="sign__title">Sign In</h1>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="sign-in__input-space">
-              <div className="p-input-icon-right w-full">
-                <i className="admss-icon-user sign__icon" />
-                <InputText placeholder='Username' className="sign__input" id="username" onChange={formik.handleChange}
-                           value={formik.values.username} />
-                {formik.errors.username ? <span>{formik.errors.username}</span> : null}
-              </div>
+        <h1 className="sign__title">Sign In</h1>
+        <form noValidate>
+          <div className="m-b-15">
+            <div className="p-input-icon-right w-full">
+              <i className="admss-icon-username sign__icon" />
+              <InputText className="sign__input" id="username" />
             </div>
+          </div>
 
-            <div className="sign-in__input-space">
-              <div className="p-input-icon-right w-full">
-                <i className="admss-icon-password sign__icon" />
-                <InputText placeholder='Password' className="sign__input" id="password" type="password" onChange={formik.handleChange}
-                           value={formik.values.password} />
-                {formik.errors.password ? <span>{formik.errors.password}</span> : null}
-              </div>
+          <div className="m-b-10">
+            <div className="p-input-icon-right w-full">
+              <i className="admss-icon-password sign__icon" />
+              <InputText className="sign__input" id="password" type="password" />
             </div>
+          </div>
 
-            <div className="flex justify-content-between user-help">
-              <div className="flex align-items-center">
-                <Checkbox inputId="remember" name="remember" checked={false} />
-                <label htmlFor="remember" className="ml-2 user-help__label">
-                  Remember me
-                </label>
-              </div>
-              <Link className="user-help__link font-semibold" to="/">
-                Forgot password?
-              </Link>
+          <div className="flex justify-content-between m-b-40 user-help">
+            <div className="flex align-items-center">
+              <Checkbox inputId="remember" name="remember" checked={false} />
+              <label htmlFor="remember" className="ml-2 user-help__label">
+                Remember me
+              </label>
             </div>
-            <div className='text-center'>
-              <Button label="Sign in" severity="success" type="submit" className="sign__button font-bold" />
-            </div>
-          </form>
-        </div>
+            <Link className="user-help__link" to="/">
+              Forgot password?
+            </Link>
+          </div>
+          <p className="text-center">
+            <Button label="Sign in" severity="success" className="sign__button" />
+          </p>
+        </form>
       </div>
     </section>
   );
