a7626a012bc97d6d161808e4bc3f86082d99a01f
Kamil Abzalov <mail@kamil-abzalov.com>
2023-08-02 23:06:44 +0300

    Create dashboard home page layout

--- src/dashboard/home/index.tsx
+++ src/dashboard/home/index.tsx
@@ -1,5 +1,93 @@
+import "./index.css";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Checkbox } from "primereact/checkbox";
+import { Calendar } from "primereact/calendar";
+import { useState } from "react";
+
 export default function Home() {
-    return <>
-        <h1>Dashboard main page</h1>
-    </>;
+  const [date, setDate] = useState(null);
+  return <>
+    <div className="card bottom-space">
+      <div className="card-header">
+        <h2 className="uppercase m-0">Common tasks</h2>
+      </div>
+    </div>
+
+    <div className="card bottom-space">
+      <div className="card-header">
+        <h2 className="uppercase m-0">Tasks</h2>
+      </div>
+      <div className="card-content">
+        <div className="flex justify-content-between">
+          <div>
+            <ul className="list-none ml-0 pl-0">
+              <li className="mb-2">
+                <Checkbox name="category" value="Add new arrivals to inventory" checked={false} />
+                <label className="ml-2">Add new arrivals to
+                  inventory</label>
+              </li>
+              <li className="mb-2">
+                <Checkbox name="category" value="Add new arrivals to inventory" checked={false} />
+                <label className="ml-2">Add new arrivals to
+                  inventory</label>
+              </li>
+              <li className="mb-2">
+                <Checkbox name="category" value="Add new arrivals to inventory" checked={false} />
+                <label className="ml-2">Add new arrivals to
+                  inventory</label>
+              </li>
+              <li className="mb-2">
+                <Checkbox name="category" value="Add new arrivals to inventory" checked={false} />
+                <label className="ml-2">Add new arrivals to
+                  inventory</label>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Calendar value={date} inline showWeek />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="flex justify-content-between">
+      <div className="card right-space">
+        <div className="card-header">
+          <h2 className="uppercase m-0">Recent messages</h2>
+        </div>
+        <div className="card-content">
+          <DataTable>
+            <Column field="from" header="From"></Column>
+            <Column field="theme" header="Theme"></Column>
+            <Column field="date" header="Date"></Column>
+          </DataTable>
+        </div>
+      </div>
+      <div className="card right-space">
+        <div className="card-header">
+          <h2 className="uppercase m-0">Recently added contact</h2>
+        </div>
+        <div className="card-content">
+          <dl>
+            <dt>Beast of Bodmin</dt>
+            <dd>A large feline inhabiting Bodmin Moor.</dd>
+          </dl>
+          <dl>
+            <dt>Beast of Bodmin</dt>
+            <dd>A large feline inhabiting Bodmin Moor.</dd>
+          </dl>
+          <dl>
+            <dt>Beast of Bodmin</dt>
+            <dd>A large feline inhabiting Bodmin Moor.</dd>
+          </dl>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <h2 className="uppercase m-0">Printing</h2>
+        </div>
+      </div>
+    </div>
+  </>;
 }
