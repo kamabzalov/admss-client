914be16ba5217a45095abe1950054a24933d5137
JSDID <icegouse@gmail.com>
2023-11-13 19:59:45 +0200

    Render task

--- src/dashboard/tasks/index.tsx
+++ src/dashboard/tasks/index.tsx
@@ -1,36 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AuthUser } from "http/services/auth.service";
 import { getKeyValue } from "services/local-storage.service";
-import { getTasksByUserId } from "http/services/tasks.service";
+import { Task, getTasksByUserId } from "http/services/tasks.service";
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
     useEffect(() => {
         const authUser: AuthUser = getKeyValue("admss-client-app-user");
         if (authUser) {
+            getTasksByUserId(authUser.useruid).then((response) => setTasks(response.splice(0, 5)));
             // eslint-disable-next-line no-console
-            getTasksByUserId(authUser.useruid).then((response) => console.log(response));
+            console.log(tasks);
         }
     }, []);
     return (
         <>
             <h2 className='card-content__title uppercase'>Tasks</h2>
             <ul className='list-none ml-0 pl-0'>
-                <li className='mb-2'>
-                    {/*<Checkbox></Checkbox>*/}
-                    <label className='ml-2'>Add new arrivals to inventory</label>
-                </li>
-                <li className='mb-2'>
-                    {/*<Checkbox></Checkbox>*/}
-                    <label className='ml-2'>Complete all daily tasks</label>
-                </li>
-                <li className='mb-2'>
-                    {/*<Checkbox></Checkbox>*/}
-                    <label className='ml-2'>Complete all daily tasks</label>
-                </li>
-                <li className='mb-2'>
-                    {/*<Checkbox></Checkbox>*/}
-                    <label className='ml-2'>Create new tasks</label>
-                </li>
+                {tasks.map((task) => (
+                    <li key={task.itemuid} className='mb-2'>
+                        <label className='ml-2'>
+                            {task.taskname && `${task.taskname} - `}
+                            {task.description && `${task.description} - `}
+                            {task.username}
+                        </label>
+                    </li>
+                ))}
             </ul>
             <span className='add-task-control font-semibold cursor-pointer'>
                 <i className='pi pi-plus add-task-control__icon'></i>
