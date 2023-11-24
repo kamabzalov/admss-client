8aece1c987141ec79edaef58ecb59d43f6625eed
JSDID <icegouse@gmail.com>
2023-11-24 19:17:59 +0200

    Add API and Task

--- src/dashboard/tasks/index.tsx
+++ src/dashboard/tasks/index.tsx
@@ -1,35 +1,149 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AuthUser } from "http/services/auth.service";
 import { getKeyValue } from "services/local-storage.service";
-import { Task, getTasksByUserId } from "http/services/tasks.service";
+import { Task, TaskStatus, getTasksByUserId, setTaskStatus } from "http/services/tasks.service";
+import { AddTaskDialog } from "./add-task-dialog";
+import { Checkbox } from "primereact/checkbox";
+import { Toast } from "primereact/toast";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 
 export const Tasks = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [showAddTaskDialog, setShowAddTaskDialog] = useState<boolean>(false);
+    const [showEditTaskDialog, setShowEditTaskDialog] = useState<boolean>(false);
+    const [checkboxDisabled, setCheckboxDisabled] = useState<boolean>(false);
+    const [currentTask, setCurrentTask] = useState<Task | null>(null);
+
+    const toast = useRef<Toast>(null);
+
+    const authUser: AuthUser = getKeyValue("admss-client-app-user");
+
+    const getTasks = () =>
+        authUser &&
+        getTasksByUserId(authUser.useruid).then((response) => setTasks(response.splice(0, 5)));
+
     useEffect(() => {
-        const authUser: AuthUser = getKeyValue("admss-client-app-user");
         if (authUser) {
-            getTasksByUserId(authUser.useruid).then((response) => setTasks(response.splice(0, 5)));
-            // eslint-disable-next-line no-console
+            getTasks();
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const handleAddTaskClick = () => {
+        setShowAddTaskDialog(true);
+    };
+
+    const handleAddTaskDialogHide = () => {
+        setShowAddTaskDialog(false);
+    };
+    const handleEditTaskDialogHide = () => {
+        setShowEditTaskDialog(false);
+    };
+
+    const handleTaskStatusChange = (taskuid: string, taskStatus: TaskStatus) => {
+        setCheckboxDisabled(true);
+        confirm(taskuid, taskStatus);
+    };
+
+    const handleEditTask = (task: Task) => {
+        setCurrentTask(task);
+        setShowEditTaskDialog(true);
+    };
+
+    const accept = (taskuid: string, taskStatus: TaskStatus) => {
+        const newStatus =
+            taskStatus === TaskStatus.COMPLETED ? TaskStatus.DEFAULT : TaskStatus.COMPLETED;
+        const detail = `The task marked as ${
+            taskStatus === TaskStatus.COMPLETED ? "uncompleted" : "completed"
+        }.`;
+        setTaskStatus(taskuid, newStatus)
+            .then((res) => {
+                if (res.status === "OK" && toast.current != null) {
+                    toast.current.show({
+                        severity: "info",
+                        summary: "Confirmed",
+                        detail,
+                        life: 3000,
+                    });
+                    getTasks();
+                }
+            })
+            .finally(() => {
+                setCheckboxDisabled(false);
+            });
+    };
+
+    const reject = () => {
+        if (toast.current != null) {
+            toast.current.show({
+                severity: "warn",
+                summary: "Rejected",
+                detail: "Action canceled!",
+                life: 3000,
+            });
+            setCheckboxDisabled(false);
+        }
+    };
+
+    const confirm = (taskuid: string, taskStatus: TaskStatus) => {
+        const message = `Are you sure you want to mark the task as ${
+            taskStatus === TaskStatus.COMPLETED ? "uncompleted" : "completed"
+        }?`;
+        confirmDialog({
+            message,
+            icon: "pi pi-exclamation-triangle",
+            accept: () => accept(taskuid, taskStatus),
+            reject,
+        });
+    };
+
     return (
         <>
             <h2 className='card-content__title uppercase'>Tasks</h2>
             <ul className='list-none ml-0 pl-0'>
                 {tasks.map((task) => (
-                    <li key={task.itemuid} className='mb-2'>
-                        <label className='ml-2'>
-                            {task.taskname && `${task.taskname} - `}
-                            {task.description && `${task.description} - `}
-                            {task.username}
+                    <li key={`${task.itemuid}-${task.index}`} className='mb-2'>
+                        <Checkbox
+                            name='task'
+                            disabled={checkboxDisabled}
+                            checked={task.task_status === TaskStatus.COMPLETED}
+                            onChange={() => handleTaskStatusChange(task.itemuid, task.task_status)}
+                        />
+                        <label
+                            className='ml-2 cursor-pointer hover:text-primary'
+                            onClick={() => handleEditTask(task)}
+                        >
+                            {task.taskname ||
+                                `${task.description} ${task.username ?? `- ${task.username}`}`}
                         </label>
                     </li>
                 ))}
             </ul>
-            <span className='add-task-control font-semibold cursor-pointer'>
+            <span
+                className='add-task-control font-semibold cursor-pointer'
+                onClick={handleAddTaskClick}
+            >
                 <i className='pi pi-plus add-task-control__icon'></i>
                 Add new task
             </span>
+            <div className='hidden'>
+                <ConfirmDialog />
+                <AddTaskDialog
+                    visible={showAddTaskDialog}
+                    onHide={handleAddTaskDialogHide}
+                    header='Add Task'
+                />
+                {currentTask && (
+                    <AddTaskDialog
+                        visible={showEditTaskDialog}
+                        onHide={handleEditTaskDialogHide}
+                        currentTask={currentTask}
+                        header='Edit Task'
+                    />
+                )}
+            </div>
+
+            <Toast ref={toast} />
         </>
     );
 };
--- src/http/services/tasks.service.ts
+++ src/http/services/tasks.service.ts
@@ -1,20 +1,37 @@
+/* eslint-disable no-unused-vars */
 import { authorizedUserApiInstance } from "http/index";
 
+export enum TaskStatus {
+    COMPLETED = "Completed",
+    STARTED = "Started",
+    DEFAULT = "Default",
+}
+
 export interface Task {
+    accountname: string;
     accountnumber: string;
     accountuid: string;
     contactname: string;
     contactuid: string;
     created: string;
     deadline: string;
+    dealname: string;
     dealuid: string;
     description: string;
     index: number;
     itemuid: string;
     parentuid: string;
     phone: string;
-    task_status: string;
+    task_status: TaskStatus;
     taskname: string;
+    updated: string;
+    username: string;
+    useruid: string;
+}
+
+export interface TaskUser {
+    created: string;
+    createdbyuid: string;
     updated: string;
     username: string;
     useruid: string;
@@ -26,4 +43,48 @@ export const getTasksByUserId = async (uid: string): Promise<Task[]> => {
         .then((response) => response.data)
         .catch((err) => err.response.data);
     return response;
+};
+
+export const createTask = async (
+    taskData: Record<string, string | number>,
+    taskuid?: string | undefined
+) => {
+    try {
+        const request = await authorizedUserApiInstance.post<any>(
+            `tasks/${taskuid || 0}/`,
+            taskData
+        );
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getTasksUserList = async (useruid: string) => {
+    try {
+        const request = await authorizedUserApiInstance.get<TaskUser[]>(`user/${useruid}/users`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const deleteTask = async (taskIndex: number) => {
+    try {
+        const request = await authorizedUserApiInstance.post<any>(`tasks/${taskIndex}/delete`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const setTaskStatus = async (taskuid: string, taskStatus: TaskStatus) => {
+    try {
+        const request = await authorizedUserApiInstance.post<any>(`tasks/${taskuid || 0}/status`, {
+            status: taskStatus,
+        });
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
 };
