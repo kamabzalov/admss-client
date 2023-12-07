3d0076e4d9f2deb7d5d5afde4bf4020c5123d860
JSDID <icegouse@gmail.com>
2023-12-07 21:22:23 +0200

    Add Task Summary

---
+++ src/dashboard/tasks/task-summary/index.tsx
@@ -0,0 +1,42 @@
+import { DialogProps } from "primereact/dialog";
+import "./index.css";
+import { Task } from "http/services/tasks.service";
+import { DashboardDialog } from "dashboard/common/dialog";
+
+interface TaskSummaryDialogProps extends DialogProps {
+    currentTask?: Task;
+}
+
+const TaskSummaryRow = ({ title, value }: { title: string; value: string }): JSX.Element => (
+    <div className='task-summary__row'>
+        <div className='task-summary__row-title'>{title}</div>
+        <span>{value}</span>
+    </div>
+);
+
+export const TaskSummaryDialog = ({
+    visible,
+    onHide,
+    header,
+    currentTask,
+}: TaskSummaryDialogProps) => {
+    return (
+        <DashboardDialog
+            onHide={onHide}
+            visible={visible}
+            header={header}
+            className={"dialog__task-summary "}
+        >
+            <div className='task-summary__body flex flex-column'>
+                <TaskSummaryRow title='Assigned to:' value={currentTask?.accountname || ""} />
+                <TaskSummaryRow title='Start date:' value={currentTask?.created || ""} />
+                <TaskSummaryRow title='Due date:' value={currentTask?.deadline || ""} />
+                <TaskSummaryRow title='Account:' value={currentTask?.accountname || ""} />
+                <TaskSummaryRow title='Deal:' value={currentTask?.dealname || ""} />
+                <TaskSummaryRow title='Contact:' value={currentTask?.contactname || ""} />
+                <TaskSummaryRow title='Phone number:' value={currentTask?.phone || ""} />
+                <TaskSummaryRow title='Description:' value={currentTask?.description || ""} />
+            </div>
+        </DashboardDialog>
+    );
+};
