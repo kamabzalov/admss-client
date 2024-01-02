05c2c5e98035bec327b00ba9c75fe9ef18a4b462
JSDID <icegouse@gmail.com>
2024-01-02 19:56:22 +0200

    Add general settings

---
+++ src/dashboard/profile/generalSettings/lease/index.tsx
@@ -0,0 +1,3 @@
+export const SettingsLease = () => {
+    return <div>Settings lease</div>;
+};
---
+++ src/dashboard/profile/generalSettings/stockTradeIn/index.tsx
@@ -0,0 +1,69 @@
+import { Checkbox } from "primereact/checkbox";
+import { InputText } from "primereact/inputtext";
+import "./index.css";
+import { DashboardRadio } from "dashboard/common/form/inputs";
+import { Slider, SliderChangeEvent } from "primereact/slider";
+import { ChangeEvent, useState } from "react";
+
+interface SettingsStockTradeInProps {
+    settings?: any;
+    radioSettings: {
+        name: string;
+        title: string;
+        value: number;
+    }[];
+}
+
+export const SettingsStockTradeIn = ({ settings, radioSettings }: SettingsStockTradeInProps) => {
+    const [value, setValue] = useState<number>(5);
+    return (
+        <div className='stock-trade-in flex flex-column gap-4'>
+            <div className='text-lg font-semibold'>Stock# for trade-in</div>
+            <div className='flex justify-content-between'>
+                <div className='flex align-items-center stock-trade-in__input'>
+                    <Checkbox inputId={settings} name={settings} value={settings} checked />
+                    <label htmlFor={settings} className='ml-2'>
+                        Sequental
+                    </label>
+                </div>
+                <div className='flex align-items-center stock-trade-in__input'>
+                    <Checkbox inputId={settings} name={settings} value={settings} checked />
+                    <label htmlFor={settings} className='ml-2'>
+                        From sold vehicle
+                    </label>
+                </div>
+            </div>
+            <div className='flex align-items-center justify-content-between'>
+                <span className='p-float-label'>
+                    <InputText id='stock-trade-in-prefix' className='stock-trade-in__input' />
+                    <label htmlFor='stock-trade-in-prefix'>Prefix</label>
+                </span>
+                <span className='p-float-label'>
+                    <InputText id='stock-trade-in-suffix' className='stock-trade-in__input' />
+                    <label htmlFor='stock-trade-in-suffix'>Suffix</label>
+                </span>
+            </div>
+            <DashboardRadio radioArray={radioSettings} />
+            <div className='flex'>
+                <label htmlFor={settings} className='ml-2'>
+                    Fixed digits
+                </label>
+                <div className='flex-1 ml-8'>
+                    <InputText
+                        value={String(value)}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setValue(Number(e.target.value))
+                        }
+                        className='w-full'
+                    />
+                    <Slider
+                        value={value}
+                        onChange={(e: SliderChangeEvent) => setValue(Number(e.value))}
+                        max={10}
+                        className='w-full'
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
