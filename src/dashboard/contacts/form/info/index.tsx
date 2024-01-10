906806ddfcd9c7d0f5ae69475c0d7131ec4c5b4c
JSDID <icegouse@gmail.com>
2024-01-10 21:15:19 +0200

    Added purchase

--- src/dashboard/contacts/form/info/index.tsx
+++ src/dashboard/contacts/form/info/index.tsx
@@ -1,10 +1,12 @@
 import { Inventory } from "dashboard/inventory/common";
+import { PurchaseFloorplan } from "./froorplan";
+import { PurchaseConsign } from "./consign";
 
 export const InventoryPurchaseData: Pick<Inventory, "label" | "items"> = {
     label: "Contact info",
     items: [
-        { itemLabel: "Floorplan" },
-        { itemLabel: "Consign" },
+        { itemLabel: "Floorplan", component: <PurchaseFloorplan /> },
+        { itemLabel: "Consign", component: <PurchaseConsign /> },
         { itemLabel: "Title" },
         { itemLabel: "Purchases" },
         { itemLabel: "Expenses" },
