fb3c3db6ce289f2fa4c13436f054846617abd391
JSDID <icegouse@gmail.com>
2024-01-15 10:07:59 +0200

    Refactoring Pagination

--- src/http/services/deals.service.ts
+++ src/http/services/deals.service.ts
@@ -1,9 +1,19 @@
 import { authorizedUserApiInstance } from "http/index";
 import { QueryParams } from "common/models/query-params";
+import { BaseResponse } from "common/models/base-response";
+
+export interface TotalDealsList extends BaseResponse {
+    total: number;
+}
 
 export const getDealsList = async (uid: string, queryParams: QueryParams) => {
     try {
-        const request = await authorizedUserApiInstance.get<any[]>(`deals/${uid}/list`, {});
+        const request = await authorizedUserApiInstance.get<any[] | TotalDealsList>(
+            `deals/${uid}/list`,
+            {
+                params: queryParams,
+            }
+        );
         return request.data;
     } catch (error) {
         // TODO: add error handler
