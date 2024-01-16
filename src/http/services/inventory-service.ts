c2623483fb657755a1eb7a410631aa049c3d673a
JSDID <icegouse@gmail.com>
2024-01-16 15:51:52 +0200

    Added API for inventory

--- src/http/services/inventory-service.ts
+++ src/http/services/inventory-service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import { AxiosResponse } from "axios";
 import { BaseResponse } from "common/models/base-response";
 import { QueryParams } from "common/models/query-params";
 import { authorizedUserApiInstance } from "http/index";
@@ -34,6 +37,22 @@ export interface TotalInventoryList extends BaseResponse {
     total: number;
 }
 
+type EndpointType =
+    | "options"
+    | "bodytypes"
+    | "exteriorcolors"
+    | "interiorcolors"
+    | "cylinders"
+    | "driveline"
+    | "engine"
+    | "expensetypes"
+    | "fueltypes"
+    | "transmissiontypes"
+    | "automakes"
+    | "category"
+    | "status"
+    | "group";
+
 export const getInventoryList = async (uid: string, queryParams: QueryParams) => {
     try {
         const request = await authorizedUserApiInstance.get<Inventory[] | TotalInventoryList>(
@@ -47,3 +66,50 @@ export const getInventoryList = async (uid: string, queryParams: QueryParams) =>
         // TODO: add error handler
     }
 };
+
+export const fetchInventoryList = async <T>(endpoint: EndpointType): Promise<T | undefined> => {
+    try {
+        const response: AxiosResponse<T> = await authorizedUserApiInstance.get(
+            `inventory/list/constants/${endpoint}`
+        );
+        return response.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export type ListData = {
+    index?: number;
+    name: string;
+};
+
+export type MakesListData = ListData & { logo: string };
+
+export const getInventoryOptionsList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("options");
+export const getInventoryBodyTypesList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("bodytypes");
+export const getInventoryExteriorColorsList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("exteriorcolors");
+export const getInventoryInteriorColorsList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("interiorcolors");
+export const getInventoryCylindersList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("cylinders");
+export const getInventoryDrivelineList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("driveline");
+export const getInventoryEngineList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("engine");
+export const getInventoryExpenseTypesList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("expensetypes");
+export const getInventoryFuelTypesList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("fueltypes");
+export const getInventoryTransmissionTypesList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("transmissiontypes");
+export const getInventoryAutomakesList = async (): Promise<MakesListData[] | undefined> =>
+    await fetchInventoryList<MakesListData[]>("automakes");
+export const getInventoryCategoryList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("category");
+export const getInventoryStatusList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("status");
+export const getInventoryGroupList = async (): Promise<ListData[] | undefined> =>
+    await fetchInventoryList<ListData[]>("group");
