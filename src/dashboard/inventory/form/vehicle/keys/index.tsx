9cd22877202be6bd28b53e5b2a6144b29a228416
JSDID <icegouse@gmail.com>
2024-01-18 19:45:30 +0200

    Added dropdown

--- src/dashboard/inventory/form/vehicle/keys/index.tsx
+++ src/dashboard/inventory/form/vehicle/keys/index.tsx
@@ -1,26 +1,34 @@
 import { ReactElement } from "react";
 import { InputText } from "primereact/inputtext";
 import { BorderedCheckbox } from "dashboard/common/form/inputs";
+import { useStore } from "store/hooks";
+import { observer } from "mobx-react-lite";
 
-export const VehicleKeys = (): ReactElement => {
+export const VehicleKeys = observer((): ReactElement => {
+    const store = useStore().inventoryStore;
+    const { inventory } = store;
     return (
         <div className='grid vehicle-keys row-gap-2'>
             <div className='col-3'>
-                <BorderedCheckbox name='Keys missing' checked={false} />
+                <BorderedCheckbox name='Keys missing' checked={!!inventory.extdata.keysMissing} />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox name='Duplicate Keys' checked={false} />
+                <BorderedCheckbox
+                    name='Duplicate Keys'
+                    checked={!!inventory.extdata.keysDuplicate}
+                />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox name='Has Remote' checked={false} />
+                <BorderedCheckbox name='Has Remote' checked={!!inventory.extdata.keysHasRemote} />
             </div>
 
             <div className='col-6'>
                 <InputText
                     placeholder='Key number/ Location'
+                    value={inventory.extdata.keyNumber}
                     className='w-full vehicle-keys__dropdown'
                 />
             </div>
         </div>
     );
-};
+});
