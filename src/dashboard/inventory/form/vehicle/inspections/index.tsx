7703b945eae6560dc83a46fefacabbe2d95fb15e
JSDID <icegouse@gmail.com>
2024-01-20 18:36:22 +0200

    Added API

--- src/dashboard/inventory/form/vehicle/inspections/index.tsx
+++ src/dashboard/inventory/form/vehicle/inspections/index.tsx
@@ -1,30 +1,78 @@
 import { ReactElement } from "react";
 import { InputText } from "primereact/inputtext";
 import { BorderedCheckbox, DateInput } from "dashboard/common/form/inputs";
+import { observer } from "mobx-react-lite";
+import { useStore } from "store/hooks";
+import { InventoryExtData } from "http/services/inventory-service";
 
-export const VehicleInspections = (): ReactElement => {
+export const VehicleInspections = observer((): ReactElement => {
+    const store = useStore().inventoryStore;
+
+    const {
+        inventoryExtData: { inspNumber, inspDate, inspEmissions, inspSafety, inspStickerExp },
+        changeInventoryExtData,
+    } = store;
+
+    const handleChange = (key: keyof InventoryExtData, value: number) => {
+        changeInventoryExtData({ key, value: !!value ? 0 : 1 });
+    };
+
     return (
         <div className='grid vehicle-inspections row-gap-2'>
             <div className='col-6'>
                 <InputText
+                    value={inspNumber}
+                    onChange={({ target: { value } }) =>
+                        changeInventoryExtData({ key: "inspNumber", value })
+                    }
                     placeholder='Inspection Number'
                     className='w-full vehicle-inspections__dropdown'
                 />
             </div>
 
             <div className='col-3'>
-                <DateInput name='Date' />
+                <DateInput
+                    value={new Date(inspDate)}
+                    onChange={({ value }) => {
+                        if (value instanceof Date) {
+                            changeInventoryExtData({
+                                key: "inspDate",
+                                value: value.getTime(),
+                            });
+                        }
+                    }}
+                    name='Date'
+                />
             </div>
             <div className='col-3'>
-                <BorderedCheckbox name='Emissions Check' checked={false} />
+                <BorderedCheckbox
+                    name='Emissions Check'
+                    checked={!!inspEmissions}
+                    onChange={() => handleChange("inspEmissions", inspEmissions)}
+                />
             </div>
 
             <div className='col-3'>
-                <BorderedCheckbox name='Safety Check' checked={false} />
+                <BorderedCheckbox
+                    name='Safety Check'
+                    checked={!!inspSafety}
+                    onChange={() => handleChange("inspSafety", inspSafety)}
+                />
             </div>
             <div className='col-3'>
-                <DateInput name='Sticker Exp. Date' />
+                <DateInput
+                    value={new Date(inspStickerExp)}
+                    onChange={({ value }) => {
+                        if (value instanceof Date) {
+                            changeInventoryExtData({
+                                key: "inspStickerExp",
+                                value: value.getTime(),
+                            });
+                        }
+                    }}
+                    name='Sticker Exp. Date'
+                />
             </div>
         </div>
     );
-};
+});
