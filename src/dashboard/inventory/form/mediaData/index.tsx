a52ca2473029fa12e76c05bc014698ef27ba0f7d
JSDID <icegouse@gmail.com>
2024-01-25 20:43:58 +0200

    Added media

--- src/dashboard/inventory/form/mediaData/index.tsx
+++ src/dashboard/inventory/form/mediaData/index.tsx
@@ -1,11 +1,24 @@
 import { Inventory } from "dashboard/inventory/common";
+import { lazy } from "react";
+const MediaDataImages = lazy(() =>
+    import("./images").then((module) => ({ default: module.ImagesMedia }))
+);
+const MediaDataVideo = lazy(() =>
+    import("./video").then((module) => ({ default: module.VideoMedia }))
+);
+const MediaDataAudio = lazy(() =>
+    import("./audio").then((module) => ({ default: module.AudioMedia }))
+);
+const MediaDataDocuments = lazy(() =>
+    import("./documents").then((module) => ({ default: module.DocumentsMedia }))
+);
 
 export const InventoryMediaData: Pick<Inventory, "label" | "items"> = {
     label: "Media data",
     items: [
-        { itemLabel: "Images" },
-        { itemLabel: "Video" },
-        { itemLabel: "Audio" },
-        { itemLabel: "Documents" },
+        { itemLabel: "Images", component: <MediaDataImages /> },
+        { itemLabel: "Video", component: <MediaDataVideo /> },
+        { itemLabel: "Audio", component: <MediaDataAudio /> },
+        { itemLabel: "Documents", component: <MediaDataDocuments /> },
     ],
 };
