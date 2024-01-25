f6f03680e51f40e1eea0fb26c346d0ad70b34f89
JSDID <icegouse@gmail.com>
2024-01-25 22:03:30 +0200

    Refactoring code

--- src/dashboard/inventory/form/mediaData/audio/index.tsx
+++ src/dashboard/inventory/form/mediaData/audio/index.tsx
@@ -1,11 +1,7 @@
 import "./index.css";
 import { ReactElement } from "react";
-// import { useStore } from "store/hooks";
 import { observer } from "mobx-react-lite";
 
 export const AudioMedia = observer((): ReactElement => {
-    // const store = useStore().inventoryStore;
-    // const { inventory, changeInventory } = store;
-
     return <div className='grid vehicle-general row-gap-2'>audio</div>;
 });
