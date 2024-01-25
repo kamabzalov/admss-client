a798f27ded5ab5aeb62fea07fa57e8541eb35dde
JSDID <icegouse@gmail.com>
2024-01-25 22:03:30 +0200

    Refactoring code

--- src/dashboard/inventory/form/mediaData/video/index.tsx
+++ src/dashboard/inventory/form/mediaData/video/index.tsx
@@ -1,11 +1,7 @@
 import "./index.css";
 import { ReactElement } from "react";
-// import { useStore } from "store/hooks";
 import { observer } from "mobx-react-lite";
 
 export const VideoMedia = observer((): ReactElement => {
-    // const store = useStore().inventoryStore;
-    // const { inventory, changeInventory } = store;
-
     return <div className='grid vehicle-general row-gap-2'>video</div>;
 });
