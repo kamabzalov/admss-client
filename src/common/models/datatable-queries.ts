0153f67d449b12f791eedaa55db953784f165262
JSDID <icegouse@gmail.com>
2024-02-01 18:25:43 +0200

    Change pager values

--- src/common/models/datatable-queries.ts
+++ src/common/models/datatable-queries.ts
@@ -1,10 +1,12 @@
+import { ROWS_PER_PAGE } from "common/settings";
+
 export interface DatatableQueries {
     [key: string]: any;
 }
 
 export const initialDataTableQueries: DatatableQueries = {
     first: 0,
-    rows: 10,
+    rows: ROWS_PER_PAGE[0],
     page: 1,
     column: "",
     sortField: "",
