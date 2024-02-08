0b2e52a2ab2a316f99e8e9c78bd81e98a8edf323
JSDID <facebook3800@ukr.net>
2024-02-08 21:44:57 +0200

    Refactoring Form

--- src/dashboard/inventory/form/mediaData/documents/index.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-import { ReactElement } from "react";
-import { observer } from "mobx-react-lite";
-
-export const DocumentsMedia = observer((): ReactElement => {
-    return <div className='grid vehicle-general row-gap-2'>documents</div>;
-});
