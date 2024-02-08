3069dbb192abd1fd63b49f8fc25c68cc8e111c95
JSDID <facebook3800@ukr.net>
2024-02-08 21:26:47 +0200

    Remove file

--- src/dashboard/inventory/form/media-data/audio/index.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-import { ReactElement } from "react";
-import { observer } from "mobx-react-lite";
-
-export const AudioMedia = observer((): ReactElement => {
-    return <div className='grid vehicle-general row-gap-2'>audio</div>;
-});
