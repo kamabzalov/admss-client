3e52649596651e25e53ce075849438f566fcb00c
Kamil Abzalov <mail@kamil-abzalov.com>
2024-02-16 21:36:27 +0200

    conflicts

--- src/dashboard/inventory/create/purchase/title/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-
-export const PurchaseTitle = (): ReactElement => (
-    <div className='grid purchase-title row-gap-2'>
-        <div className='col-6'></div>
-    </div>
-);
