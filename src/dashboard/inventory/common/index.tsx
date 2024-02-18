596b637d21f6d7abf5ea0abf73438761c9604f26
JSDID <facebook3800@ukr.net>
2024-02-18 20:24:10 +0200

    Added new column

--- src/dashboard/inventory/common/index.tsx
+++ src/dashboard/inventory/common/index.tsx
@@ -28,34 +28,38 @@ export class InventorySection implements Inventory {
     public constructor({ label, items }: { label: string; items: InventoryItem[] }) {
         this.sectionId = ++InventorySection.instancesCount;
         this.label = label;
-        this.items = items.map(({ itemLabel, component }) => ({
+        this.items = items.map(({ itemLabel, component }, index: number) => ({
             itemLabel,
             component,
             itemIndex: InventorySection.itemIndex++,
-            template: (item: MenuItem, options: MenuItemOptions) => this.newTemplate(item, options),
+            template: (item: MenuItem, options: MenuItemOptions) => {
+                return this.newTemplate(item, options, index);
+            },
         }));
         this.startIndex = InventorySection.itemIndex - this.items.length;
     }
 
-    private newTemplate(item: MenuItem, options: MenuItemOptions): JSX.Element {
+    private newTemplate(
+        item: MenuItem,
+        { props, onClick, className, labelClassName }: MenuItemOptions,
+        index: number
+    ): JSX.Element {
+        const isGreen =
+            (InventorySection.instancesCount > this.items.length || index <= props.activeIndex) &&
+            props.activeIndex !== 0;
+
         return (
             <a
                 href='#'
-                role='presentation'
-                data-pc-section='action'
-                onClick={options.onClick}
-                className={`${options.className} vertical-nav flex-row align-items-center justify-content-start w-full`}
+                onClick={onClick}
+                className={`${className} vertical-nav flex-row align-items-center justify-content-start w-full`}
             >
                 <label
-                    className={"vertical-nav__icon p-steps-number border-circle "}
-                    data-pc-section='step'
+                    className={`vertical-nav__icon p-steps-number ${
+                        isGreen && "p-steps-number--green"
+                    } border-circle`}
                 />
-                <span
-                    className={`${options.labelClassName} vertical-nav__label`}
-                    data-pc-section='label'
-                >
-                    {item.label}
-                </span>
+                <span className={`${labelClassName} vertical-nav__label`}>{item.label}</span>
             </a>
         );
     }
