934a3be77d005a6077fc63339162fad0534238b5
JSDID <facebook3800@ukr.net>
2024-02-19 15:20:02 +0200

    Refactoring contact

--- src/dashboard/contacts/form/contact-info/contacts/index.tsx
+++ src/dashboard/contacts/form/contact-info/contacts/index.tsx
@@ -2,41 +2,32 @@ import { observer } from "mobx-react-lite";
 import { InputText } from "primereact/inputtext";
 import { ReactElement } from "react";
 import { Button } from "primereact/button";
-import { useStore } from "store/hooks";
 
 export const ContactsSocialInfo = observer((): ReactElement => {
-    const store = useStore().contactStore;
-    const { contact } = store;
     return (
         <div className='grid contacts-social row-gap-2'>
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        value={contact.email1}
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>E-mail</label>
                 </span>
             </div>
             <div className='col-6'>
                 <Button className='w-full'>
-                    <i className='pi pi-plus mr-2 text-xs pt-1' />
+                    <i className='icon adms-add-item-round mr-2' />
                     Add another email address
                 </Button>
             </div>
 
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        value={contact.phone1}
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>Phone Number</label>
                 </span>
             </div>
             <div className='col-6'>
                 <Button className='w-full'>
-                    <i className='pi pi-plus mr-2 text-xs pt-1' />
+                    <i className='icon adms-add-item-round mr-2' />
                     Add another phone number
                 </Button>
             </div>
@@ -45,39 +36,27 @@ export const ContactsSocialInfo = observer((): ReactElement => {
 
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        //TODO: missing social values
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>Facebook</label>
                 </span>
             </div>
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        //TODO: missing social values
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>WhatsApp</label>
                 </span>
             </div>
 
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        //TODO: missing social values
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>Slack</label>
                 </span>
             </div>
 
             <div className='col-6'>
                 <span className='p-float-label'>
-                    <InputText
-                        className='contacts-social__text-input w-full'
-                        //TODO: missing social values
-                    />
+                    <InputText className='contacts-social__text-input w-full' />
                     <label className='float-label'>Skype</label>
                 </span>
             </div>
--- src/dashboard/contacts/form/contact-info/prospecting/index.tsx
+++ src/dashboard/contacts/form/contact-info/prospecting/index.tsx
@@ -1,31 +1,12 @@
 import { observer } from "mobx-react-lite";
 import { Dropdown } from "primereact/dropdown";
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import "./index.css";
 import { DateInput } from "dashboard/common/form/inputs";
 import { Button } from "primereact/button";
 import { InputTextarea } from "primereact/inputtextarea";
-import { useStore } from "store/hooks";
-import { useParams } from "react-router-dom";
-import { ContactType } from "common/models/contact";
-import { getContactsSalesmanList } from "http/services/contacts-service";
 
 export const ContactsProspecting = observer((): ReactElement => {
-    const { id } = useParams();
-
-    const store = useStore().contactStore;
-    const { contact } = store;
-    const [salesperson, setSalesperson] = useState<string>("");
-    const [salespersonsList, setSalespersonsList] = useState<ContactType[]>([]);
-
-    useEffect(() => {
-        if (id) {
-            getContactsSalesmanList(id).then((response) => {
-                response && setSalespersonsList(response);
-            });
-        }
-    }, [id]);
-
     return (
         <div className='grid contacts-prospecting row-gap-2'>
             <div className='col-6'>
@@ -33,11 +14,6 @@ export const ContactsProspecting = observer((): ReactElement => {
                     optionLabel='name'
                     optionValue='name'
                     filter
-                    //TODO: missing init value
-                    value={salesperson}
-                    //TODO: API list is not working
-                    options={salespersonsList}
-                    onChange={({ target: { value } }) => setSalesperson(value)}
                     placeholder='Attending Salesman'
                     className='w-full contacts-prospecting__dropdown'
                 />
@@ -46,7 +22,6 @@ export const ContactsProspecting = observer((): ReactElement => {
             <div className='col-6'>
                 <DateInput
                     placeholder='Contact till...'
-                    //TODO: missing value
                     className='contacts-prospecting__date-input w-full'
                 />
             </div>
@@ -56,14 +31,13 @@ export const ContactsProspecting = observer((): ReactElement => {
                     optionLabel='name'
                     optionValue='name'
                     filter
-                    //TODO: missing value
                     placeholder='Choose a Vehicle'
                     className='w-full contacts-prospecting__dropdown'
                 />
             </div>
             <div className='col-6'>
                 <Button className='w-full'>
-                    <i className='pi pi-plus mr-2 text-xs pt-1' />
+                    <i className='icon adms-add-item-round mr-2' />
                     Add another Vehicle
                 </Button>
             </div>
@@ -71,11 +45,10 @@ export const ContactsProspecting = observer((): ReactElement => {
             <div className='col-12'>
                 <InputTextarea
                     placeholder='Prospecting Notes'
-                    value={contact.extdata.Notes}
                     className='w-full contacts-prospecting__text-area'
                 />
             </div>
-            <div className='col-12 flex justify-content-end '>
+            <div className='col-12'>
                 <Button className='px-4'>Schedule Call Back</Button>
             </div>
         </div>
