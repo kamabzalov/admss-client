0e4f272f8278e00b7195068304d27c5ba3162d2f
JSDID <facebook3800@ukr.net>
2024-03-22 17:19:18 +0200

    Added set user settings

--- src/http/services/auth-user.service.ts
+++ src/http/services/auth-user.service.ts
@@ -1,15 +1,9 @@
-import { BaseResponse } from "common/models/base-response";
 import { authorizedUserApiInstance } from "../index";
 
-export interface ExtendedUserData extends BaseResponse {
+export interface ExtendedUserData {
     location: string;
     dealerName: string;
-}
-
-export interface UserSettings extends BaseResponse {
-    created: string;
-    profile: string;
-    updated: string;
+    status: "OK";
 }
 
 export const getExtendedData = async (uid: string) => {
@@ -23,7 +17,7 @@ export const getExtendedData = async (uid: string) => {
 
 export const getUserSettings = async (uid: string) => {
     try {
-        const request = await authorizedUserApiInstance.get<UserSettings>(`user/${uid}/profile`);
+        const request = await authorizedUserApiInstance.get<any>(`user/${uid}/profile`);
         return request.data;
     } catch (error) {
         // TODO: add error handler
@@ -32,9 +26,7 @@ export const getUserSettings = async (uid: string) => {
 
 export const setUserSettings = async (uid: string, settings: any) => {
     try {
-        const request = await authorizedUserApiInstance.post<any>(`user/${uid}/profile`, {
-            profile: JSON.stringify(settings),
-        });
+        const request = await authorizedUserApiInstance.post<any>(`user/${uid}/profile`, settings);
         return request.data;
     } catch (error) {
         // TODO: add error handler
