837736987bd3ca3404e14f2c2391e61667a12c70
JSDID <facebook3800@ukr.net>
2024-04-15 08:05:21 +0300

    Added deal API service

--- src/dashboard/deals/form/retail/charges/index.tsx
+++ src/dashboard/deals/form/retail/charges/index.tsx
@@ -1,7 +1,20 @@
 import { observer } from "mobx-react-lite";
 import { ReactElement } from "react";
 import "./index.css";
+import { CurrencyInput } from "dashboard/common/form/inputs";
 
 export const DealRetailCharges = observer((): ReactElement => {
-    return <div className='grid deal-retail-charges row-gap-2'></div>;
+    return (
+        <div className='grid deal-retail-charges row-gap-2'>
+            <div className='col-3'>
+                <CurrencyInput labelPosition='top' title='Credit Life' />
+            </div>
+            <div className='col-3'>
+                <CurrencyInput labelPosition='top' title='A&H/ Disability' />
+            </div>
+            <div className='col-3'>
+                <CurrencyInput labelPosition='top' title='VSI' />
+            </div>
+        </div>
+    );
 });
