a97fbab151ad0ee4db967d5822170d66c1fc214f
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-04-29 13:09:19 +0300

    Added icon

--- src/dashboard/inventory/form/media-data/video/index.tsx
+++ src/dashboard/inventory/form/media-data/video/index.tsx
@@ -139,14 +139,9 @@ export const VideoMedia = observer((): ReactElement => {
         return (
             <div className='flex align-items-center presentation'>
                 <div className='flex align-items-center'>
-                    <img
-                        alt={file.name}
-                        src={URL.createObjectURL(file)}
-                        role='presentation'
-                        width={29}
-                        height={29}
-                        className='presentation__video'
-                    />
+                    <div className='media-preview'>
+                        <i className='icon adms-play-prev media-preview__icon' />
+                    </div>
                     <span className='presentation__label flex flex-column text-left ml-3'>
                         {file.name}
                     </span>
