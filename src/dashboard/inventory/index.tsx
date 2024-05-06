a41dc47272fe7514a464901b8dc5ebb0a73ab259
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-05-06 17:05:15 +0300

    Changed styles

--- src/dashboard/inventory/index.tsx
+++ src/dashboard/inventory/index.tsx
@@ -531,9 +531,11 @@ export default function Inventories(): ReactElement {
     return (
         <div className='grid'>
             <div className='col-12'>
-                <div className='card'>
+                <div className='card inventory'>
                     <div className='card-header'>
-                        <h2 className='card-header__title uppercase m-0'>Inventory</h2>
+                        <h2 className='card-header__title inventory__title uppercase m-0'>
+                            Inventory
+                        </h2>
                         {locations.length > 0 && (
                             <SplitButton
                                 label={currentLocation?.locName || "Select Location"}
@@ -543,7 +545,12 @@ export default function Inventories(): ReactElement {
                                     command: () => setCurrentLocation(location),
                                 }))}
                                 rounded
-                                menuStyle={{ transform: "translateX(150px)" }}
+                                menuStyle={{ transform: "translateX(164px)" }}
+                                pt={{
+                                    menu: {
+                                        className: "inventory-location__menu",
+                                    },
+                                }}
                             />
                         )}
                     </div>
