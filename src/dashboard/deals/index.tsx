e7d5849eaf18ae10b14221bff9adaba8b15ef537
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-05-10 19:26:41 +0300

    changed filter

--- src/dashboard/deals/index.tsx
+++ src/dashboard/deals/index.tsx
@@ -50,7 +50,6 @@ const DEALS_OTHER_LIST: DealsFilterOptions[] = [
     { name: "All incomplete", value: "0.DealComplete" },
     { name: "Dead or Deleted", value: "6.DealStatus" },
     { name: "Manager's review", value: "1.managerReview" },
-    { name: "Deals not yet sent to RFC", value: "0.RFCSent" },
     { name: "Deals not yet sent to RFC", value: "0.RFCSen" },
 ];
 
