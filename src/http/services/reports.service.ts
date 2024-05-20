9d7776f17b3cfd1b4d69a4025a43f09bfa948ae7
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-05-20 18:20:11 +0300

    Added reports

--- src/http/services/reports.service.ts
+++ src/http/services/reports.service.ts
@@ -1,8 +1,9 @@
+import { BaseResponse } from "common/models/base-response";
 import { QueryParams } from "common/models/query-params";
 import { ReportsPostData } from "common/models/reports";
 import { authorizedUserApiInstance } from "http/index";
 
-export const getReportsList = async (uid: string, queryParams: QueryParams) => {
+export const getReportsList = async (uid: string, queryParams?: QueryParams) => {
     try {
         const request = await authorizedUserApiInstance.get<any[]>(`reports/${uid}/list`, {
             params: queryParams,
@@ -13,9 +14,90 @@ export const getReportsList = async (uid: string, queryParams: QueryParams) => {
     }
 };
 
+export const getReportCollection = async (uid: string) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/${uid}/collection`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getUserReportCollections = async (uid: string) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/${uid}/collections`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getSpecificDocument = async (uid: string, documentId: string) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any>(`reports/${uid}/${documentId}`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getReportTemplate = async (uid: string) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any>(`reports/${uid}/get`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getCommonReportsList = async () => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/list`);
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getCommonReportsListByState = async (state: string, queryParams?: QueryParams) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/${state}/list`, {
+            params: queryParams,
+        });
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getTasksList = async (queryParams?: QueryParams) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/tasks`, {
+            params: queryParams,
+        });
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
+export const getQueueList = async (queryParams?: QueryParams) => {
+    try {
+        const request = await authorizedUserApiInstance.get<any[]>(`reports/queue`, {
+            params: queryParams,
+        });
+        return request.data;
+    } catch (error) {
+        // TODO: add error handler
+    }
+};
+
 export const makeReports = async (uid: string | undefined, body?: ReportsPostData) => {
     try {
-        const request = await authorizedUserApiInstance.post<any>(`reports/${uid}/report`, body);
+        const request = await authorizedUserApiInstance.post<BaseResponse>(
+            `reports/${uid}/report`,
+            body
+        );
         return request.data;
     } catch (error) {
         // TODO: add error handler
