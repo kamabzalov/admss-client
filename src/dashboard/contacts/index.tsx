510ed713731d587f0792004a25916b53a9696499
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-05-24 15:32:55 +0300

    Changed export and contacts

--- src/dashboard/contacts/index.tsx
+++ src/dashboard/contacts/index.tsx
@@ -169,7 +169,14 @@ export const ContactsDataTable = ({ onRowClick, contactCategory }: ContactsDataT
         if (authUser) {
             getUserSettings(authUser.useruid).then((response) => {
                 if (response?.profile.length) {
-                    const allSettings: ServerUserSettings = JSON.parse(response.profile);
+                    let allSettings: ServerUserSettings = {} as ServerUserSettings;
+                    if (response.profile) {
+                        try {
+                            allSettings = JSON.parse(response.profile);
+                        } catch (error) {
+                            allSettings = {} as ServerUserSettings;
+                        }
+                    }
                     setServerSettings(allSettings);
                     const { contacts: settings } = allSettings;
                     settings?.activeColumns &&
