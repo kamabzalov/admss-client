60b46b505424d7e1025a77b8592e3320dff25e52
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-05-28 19:23:03 +0300

    Changed reports and export protected

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,123 @@
 import { Router as RemixRouter } from "@remix-run/router/dist/router";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { ReactElement, useEffect, useState } from "react";
-import { AuthUser } from "http/services/auth.service";
-import { LS_APP_USER } from "common/constants/localStorage";
-import { getKeyValue } from "services/local-storage.service";
-import { salespersonRoutes } from "http/routes/salesperson";
-import { adminRoutes } from "http/routes/admin";
+import { ReactElement } from "react";
+import App from "App";
+import Dashboard from "dashboard";
+import Accounts from "dashboard/accounts";
+import { AccountsForm } from "dashboard/accounts/form";
+import Contacts from "dashboard/contacts";
+import { ContactForm } from "dashboard/contacts/form";
+import Deals from "dashboard/deals";
+import { DealsForm } from "dashboard/deals/form";
+import Home from "dashboard/home";
+import Inventory from "dashboard/inventory";
+import { InventoryForm } from "dashboard/inventory/form";
+import NotFound from "not-found";
+import SignIn from "sign/sign-in";
+import ProtectedRoute from "http/routes/ProtectedRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 const AppRouter = (): ReactElement => {
-    const [isSalesPerson, setIsSalesPerson] = useState<boolean>(false);
-
-    useEffect(() => {
-        const authUser: AuthUser = getKeyValue(LS_APP_USER);
-        if (authUser) {
-            setIsSalesPerson(!!authUser?.issalesperson);
-        }
-    }, []);
-
-    const routes = isSalesPerson ? salespersonRoutes : adminRoutes;
+    const routes = [
+        {
+            path: "/",
+            element: <App />,
+            errorElement: <NotFound />,
+            children: [
+                { path: "", element: <SignIn /> },
+                {
+                    path: "dashboard",
+                    element: (
+                        <ProtectedRoute allowedRoles={["admin", "salesPerson"]}>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    ),
+                    children: [
+                        { path: "", element: <Home /> },
+                        {
+                            path: "inventory",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin", "salesPerson"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                            children: [
+                                { path: "", element: <Inventory /> },
+                                { path: "create", element: <InventoryForm /> },
+                                { path: ":id", element: <InventoryForm /> },
+                            ],
+                        },
+                        {
+                            path: "contacts",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                            children: [
+                                { path: "", element: <Contacts /> },
+                                { path: "create", element: <ContactForm /> },
+                                { path: ":id", element: <ContactForm /> },
+                            ],
+                        },
+                        {
+                            path: "deals",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                            children: [
+                                { path: "", element: <Deals /> },
+                                { path: "create", element: <DealsForm /> },
+                                { path: ":id", element: <DealsForm /> },
+                            ],
+                        },
+                        {
+                            path: "accounts",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                            children: [
+                                { path: "", element: <Accounts /> },
+                                { path: "create", element: <AccountsForm /> },
+                                { path: ":id", element: <AccountsForm /> },
+                            ],
+                        },
+                        {
+                            path: "settings",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                        },
+                        {
+                            path: "reports",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                        },
+                        {
+                            path: "export-web",
+                            element: (
+                                <ProtectedRoute allowedRoles={["admin"]}>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            ),
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
 
     const router: RemixRouter = createBrowserRouter(routes);
 
