cb42fac03de679c0ab1a8a6afba31d875ff8bfec
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-06-17 16:33:42 +0300

    Removed else statement

--- src/dashboard/reports/index.tsx
+++ src/dashboard/reports/index.tsx
@@ -1,61 +1,36 @@
 import { ReactElement, useEffect, useState } from "react";
 import { AuthUser } from "http/services/auth.service";
-import { getCommonReportsList, getReportById, getReportsList } from "http/services/reports.service";
+import {
+    createReportCollection,
+    getReportsList,
+    getUserReportCollectionsContent,
+} from "http/services/reports.service";
 import { Button } from "primereact/button";
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { InputText } from "primereact/inputtext";
 import "./index.css";
 import { getKeyValue } from "services/local-storage.service";
 import { LS_APP_USER } from "common/constants/localStorage";
-import { BaseResponseError, Status } from "common/models/base-response";
+import { BaseResponseError } from "common/models/base-response";
 import { useToast } from "dashboard/common/toast";
 import { TOAST_LIFETIME } from "common/settings";
+import { Panel, PanelHeaderTemplateOptions } from "primereact/panel";
+import { MultiSelect } from "primereact/multiselect";
+import { ReportCollectionContent, ReportDocument } from "common/models/reports";
 
-const mockReportsGroup = [
-    {
-        id: 1,
-        name: "Favorites",
-    },
-    {
-        id: 2,
-        name: "AR Reports",
-    },
-    {
-        id: 3,
-        name: "BHPH Reports",
-    },
-    {
-        id: 4,
-        name: "Custom Collections",
-    },
-    {
-        id: 5,
-        name: "Custom Reports",
-    },
-    {
-        id: 6,
-        name: "Inventory Reports",
-    },
-    {
-        id: 7,
-        name: "Miscellaneous",
-    },
-    {
-        id: 8,
-        name: "Sales Reports",
-    },
-];
-
-const getMockReports = () => {
-    return Array.from({ length: Math.random() * 7 + 1 }, (_, i) => ({
-        id: i.toString(),
-        name: `report ${i + 1}`,
-    }));
-};
+interface Report {
+    id: string;
+    name: string;
+}
 
 export default function Reports(): ReactElement {
     const [user, setUser] = useState<AuthUser | null>(null);
     const [reportSearch, setReportSearch] = useState<string>("");
+    // eslint-disable-next-line
+    const [reports, setReports] = useState<ReportDocument[]>([]);
+    const [collections, setCollections] = useState<ReportCollectionContent[]>([]);
+    const [collectionName, setCollectionName] = useState<string>("");
+    const [selectedReports, setSelectedReports] = useState<Report[]>([]);
 
     const toast = useToast();
 
@@ -64,23 +39,45 @@ export default function Reports(): ReactElement {
         setUser(authUser);
     }, []);
 
+    const handleGetReportList = (useruid: string) =>
+        getReportsList(useruid).then((response) => {
+            const { error } = response as BaseResponseError;
+            if (error && toast.current) {
+                return toast.current.show({
+                    severity: "error",
+                    summary: "Error",
+                    detail: error,
+                    life: TOAST_LIFETIME,
+                });
+            }
+            const document = response as ReportDocument[];
+            setReports(document);
+        });
+
+    const handleGetUserReportCollections = (useruid: string) =>
+        getUserReportCollectionsContent(useruid).then((response) => {
+            const { error } = response as BaseResponseError;
+            if (error && toast.current) {
+                return toast.current.show({
+                    severity: "error",
+                    summary: "Error",
+                    detail: error,
+                    life: TOAST_LIFETIME,
+                });
+            }
+            const collections = response as ReportCollectionContent[];
+            setCollections(collections);
+        });
+
     useEffect(() => {
         if (user) {
-            getReportsList(user.useruid, { total: 1 }).then((response) => {});
-            getCommonReportsList().then((response) => {
-                if (response.status === Status.ERROR && toast.current) {
-                    const { error } = response as BaseResponseError;
-                    toast.current.show({
-                        severity: "error",
-                        summary: "Error",
-                        detail: error,
-                        life: TOAST_LIFETIME,
-                    });
-                }
-            });
+            handleGetReportList(user.useruid);
+            handleGetUserReportCollections(user.useruid);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [toast, user]);
 
+    // eslint-disable-next-line
     const ActionButtons = ({ reportuid }: { reportuid: string }): ReactElement => {
         return (
             <div className='reports-actions flex gap-3'>
@@ -111,6 +108,68 @@ export default function Reports(): ReactElement {
         );
     };
 
+    const ReportsPanelHeader = (options: PanelHeaderTemplateOptions) => {
+        return (
+            <div className='reports-header col-12 px-0 pb-3'>
+                <Button
+                    icon='pi pi-plus'
+                    className='reports-header__button'
+                    onClick={options.onTogglerClick}
+                >
+                    New collection
+                </Button>
+                <Button className='reports-header__button'>Custom Report</Button>
+                <span className='p-input-icon-right reports-header__search'>
+                    <i
+                        className={`pi pi-${!reportSearch ? "search" : "times cursor-pointer"}`}
+                        onClick={() => setReportSearch("")}
+                    />
+                    <InputText
+                        value={reportSearch}
+                        placeholder='Search'
+                        onChange={(e) => setReportSearch(e.target.value)}
+                    />
+                </span>
+            </div>
+        );
+    };
+
+    // eslint-disable-next-line
+    const handleReportGroupSelect = (items: Report[]) => {
+        const allItemsSelected = items.every((item) =>
+            selectedReports.some((selected) => selected.id === item.id)
+        );
+
+        if (allItemsSelected) {
+            setSelectedReports(
+                selectedReports.filter((report) => !items.some((item) => item.id === report.id))
+            );
+        } else {
+            const newSelectedReports = items.filter(
+                (item) => !selectedReports.some((selected) => selected.id === item.id)
+            );
+            setSelectedReports([...selectedReports, ...newSelectedReports]);
+        }
+    };
+
+    const handleCreateCollection = () => {
+        if (collectionName) {
+            createReportCollection(user!.useruid, collectionName).then((response) => {
+                const { error } = response as BaseResponseError;
+                if (error && toast.current) {
+                    toast.current.show({
+                        severity: "error",
+                        summary: "Error",
+                        detail: error,
+                        life: TOAST_LIFETIME,
+                    });
+                } else {
+                    user && handleGetUserReportCollections(user.useruid);
+                }
+            });
+        }
+    };
+
     return (
         <div className='grid'>
             <div className='col-12'>
@@ -120,57 +179,71 @@ export default function Reports(): ReactElement {
                     </div>
                     <div className='card-content'>
                         <div className='grid'>
-                            <div className='reports-header col-12'>
-                                <Button icon='pi pi-plus' className='reports-header__button'>
-                                    New collection
-                                </Button>
-                                <Button className='reports-header__button'>Custom Report</Button>
-                                <span className='p-input-icon-right reports-header__search'>
-                                    <i
-                                        className={`pi pi-${
-                                            !reportSearch ? "search" : "times cursor-pointer"
-                                        }`}
-                                        onClick={() => setReportSearch("")}
-                                    />
-                                    <InputText
-                                        value={reportSearch}
-                                        placeholder='Search'
-                                        onChange={(e) => setReportSearch(e.target.value)}
-                                    />
-                                </span>
+                            <div className='col-12'>
+                                <Panel
+                                    headerTemplate={ReportsPanelHeader}
+                                    className='new-collection w-full'
+                                    collapsed
+                                    toggleable
+                                >
+                                    <h3 className='uppercase new-collection__title'>
+                                        Add new collection
+                                    </h3>
+                                    <div className='grid new-collection__form'>
+                                        <div className='col-4'>
+                                            <InputText
+                                                className='w-full'
+                                                value={collectionName}
+                                                onChange={(e) => setCollectionName(e.target.value)}
+                                                placeholder='Collection name'
+                                            />
+                                        </div>
+                                        <div className='col-8'>
+                                            <MultiSelect
+                                                filter
+                                                optionLabel='name'
+                                                options={reports}
+                                                className='w-full new-collection__multiselect'
+                                                placeholder='Select reports'
+                                                showSelectAll={false}
+                                                value={selectedReports}
+                                                display='chip'
+                                                onChange={(e) => {
+                                                    e.stopPropagation();
+                                                    setSelectedReports(e.value);
+                                                }}
+                                                pt={{
+                                                    wrapper: {
+                                                        style: {
+                                                            minHeight: "420px",
+                                                        },
+                                                    },
+                                                }}
+                                            />
+                                        </div>
+                                        <div className='col-12 flex justify-content-end'>
+                                            <Button onClick={handleCreateCollection} outlined>
+                                                Create collection
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </Panel>
                             </div>
                             <div className='col-12'>
-                                <Accordion
-                                    multiple
-                                    activeIndex={[0]}
-                                    className='reports__accordion'
-                                >
-                                    {mockReportsGroup.map((group) => {
-                                        const mockReports = getMockReports();
-                                        return (
+                                <Accordion multiple className='reports__accordion'>
+                                    {collections &&
+                                        collections.map(({ itemUID, name }) => (
                                             <AccordionTab
-                                                key={group.id}
+                                                key={itemUID}
                                                 header={
                                                     <ReportsAccordionHeader
-                                                        title={group.name}
-                                                        info={`(${mockReports.length} reports)`}
+                                                        title={name}
+                                                        info={`(0 reports)`}
                                                     />
                                                 }
                                                 className='reports__accordion-tab'
-                                            >
-                                                {mockReports.map((report) => (
-                                                    <div
-                                                        className='reports__list-item'
-                                                        key={report.id}
-                                                        onClick={() => getReportById(report.id)}
-                                                    >
-                                                        <p>{`${group.name} ${report.name}`}</p>
-                                                        <ActionButtons reportuid={report.id} />
-                                                    </div>
-                                                ))}
-                                            </AccordionTab>
-                                        );
-                                    })}
+                                            ></AccordionTab>
+                                        ))}
                                 </Accordion>
                             </div>
                         </div>
