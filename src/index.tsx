0d4939cedf35266026022c831479abd136f31447
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-06-25 21:56:00 +0300

    Changed Router

--- src/index.tsx
+++ src/index.tsx
@@ -33,16 +33,13 @@ const AppRouter = (): ReactElement => {
                 { path: "", element: <SignIn /> },
                 {
                     path: "dashboard",
-                    element: <Dashboard />,
+                    element: (
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    ),
                     children: [
-                        {
-                            path: "",
-                            element: (
-                                <ProtectedRoute>
-                                    <Home />
-                                </ProtectedRoute>
-                            ),
-                        },
+                        { path: "", element: <Home /> },
                         {
                             path: "inventory",
                             element: (
@@ -124,6 +121,7 @@ const AppRouter = (): ReactElement => {
             ],
         },
     ];
+
 
     const router: RemixRouter = createBrowserRouter(routes);
 
