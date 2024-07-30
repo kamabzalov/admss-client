cf0fbef981afc64d76f019c24449dfe5c4871d16
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-07-30 20:17:34 +0300

    Delete comment

--- src/dashboard/accounts/take-payment-form/quick-pay/index.tsx
+++ src/dashboard/accounts/take-payment-form/quick-pay/index.tsx
@@ -1,10 +1,8 @@
 import { ReactElement } from "react";
-// import { useParams } from "react-router-dom";
 
 import { observer } from "mobx-react-lite";
 
 export const AccountQuickPay = observer((): ReactElement => {
-    // const { id } = useParams();
 
     return <div className='quick-pay'>Quick Pay</div>;
 });
