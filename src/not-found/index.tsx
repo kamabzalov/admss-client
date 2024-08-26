43c8ed530d00204a7f5c09ca0be91d1977cf04d5
Vladyslav Vlasov <vv524519@gmail.com>
2024-08-26 21:56:11 +0300

    Added 404-error page.

--- src/not-found/index.tsx
+++ src/not-found/index.tsx
@@ -1,3 +1,24 @@
-export default function NotFound() {
-    return <h1>Not found</h1>;
-}
+import React from "react";
+import "./index.css";
+import notFoundImage from "../not-found/404/404 page not found (1).png";
+
+const NotFound = () => {
+    return (
+        <div className='not-found-container'>
+            <div className='not-found-content'>
+                <h2 className='not-found-header'>OOPS!</h2>
+                <h1 className='not-found-title'>404</h1>
+                <p className='not-found-message'>Page Not Found</p>
+                <p className='not-found-description'>
+                    The requested URL was not found on this server
+                </p>
+                <div className='not-found-buttons'>
+                    <button className='back-button'>Go Back</button>
+                    <button className='contact-button'>Contact Us</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
