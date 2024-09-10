0349ea186401b0a7fbf486bc268b738b19520517
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-09-10 18:02:04 +0300

    Added breakdown total paid

--- src/dashboard/accounts/take-payment-form/quick-pay/total-amount/index.tsx
+++ src/dashboard/accounts/take-payment-form/quick-pay/total-amount/index.tsx
@@ -5,13 +5,34 @@ import { Button } from "primereact/button";
 import { Dropdown } from "primereact/dropdown";
 import { InputText } from "primereact/inputtext";
 import { ReactElement } from "react";
+import { useStore } from "store/hooks";
 
 export const AccountTotalAmount = observer((): ReactElement => {
+    const store = useStore().accountStore;
+    const {
+        accountTakePayment: {
+            PaymentDate,
+            PaymentMethod,
+            CheckNumber,
+            TotalAmount,
+            BreakdownDownPayment,
+            BreakdownFees,
+            BreakdownPrincipal,
+            BreakdownContractPayment,
+        },
+        changeAccountTakePayment,
+    } = store;
     return (
         <div className='take-payment__card'>
             <div className='take-payment__item'>
                 <label className='take-payment__label'>Payment Date</label>
-                <DateInput className='take-payment__input' />
+                <DateInput
+                    className='take-payment__input'
+                    date={PaymentDate}
+                    onChange={(e) =>
+                        changeAccountTakePayment("PaymentDate", e.target.value as string)
+                    }
+                />
             </div>
 
             <div className='take-payment__item'>
@@ -19,6 +40,7 @@ export const AccountTotalAmount = observer((): ReactElement => {
                 <Dropdown
                     id='pmtMethod'
                     options={ACCOUNT_PAYMENT_METHODS}
+                    value={PaymentMethod}
                     optionValue='id'
                     optionLabel='name'
                 />
@@ -26,7 +48,13 @@ export const AccountTotalAmount = observer((): ReactElement => {
 
             <div className='take-payment__item'>
                 <label className='take-payment__label'>Check#</label>
-                <InputText id='checkNumber' />
+                <InputText
+                    id='checkNumber'
+                    value={CheckNumber}
+                    onChange={(e) => {
+                        changeAccountTakePayment("CheckNumber", e.target.value);
+                    }}
+                />
             </div>
 
             <hr className='form-line' />
@@ -34,7 +62,13 @@ export const AccountTotalAmount = observer((): ReactElement => {
             <div className='quick-pay__total'>
                 <h3 className='quick-pay__title color-green'>Total Amount Paid:</h3>
                 <div className='quick-pay__input'>
-                    <CurrencyInput className='quick-pay__total-input' value={0} />
+                    <CurrencyInput
+                        className='quick-pay__total-input'
+                        value={TotalAmount}
+                        onChange={({ value }) => {
+                            changeAccountTakePayment("TotalAmount", value as number);
+                        }}
+                    />
                     <Button severity='secondary' icon='pi pi-arrow-right' />
                 </div>
             </div>
@@ -45,19 +79,39 @@ export const AccountTotalAmount = observer((): ReactElement => {
 
             <div className='take-payment__item color-dusty-blue'>
                 <label className='take-payment__label'>Down/ Pickup:</label>
-                <CurrencyInput value={0} />
+                <CurrencyInput
+                    value={BreakdownDownPayment}
+                    onChange={({ value }) => {
+                        changeAccountTakePayment("BreakdownDownPayment", value as number);
+                    }}
+                />
             </div>
             <div className='take-payment__item color-red'>
                 <label className='take-payment__label'>Fees:</label>
-                <CurrencyInput value={0} />
+                <CurrencyInput
+                    value={BreakdownFees}
+                    onChange={({ value }) => {
+                        changeAccountTakePayment("BreakdownFees", value as number);
+                    }}
+                />
             </div>
             <div className='take-payment__item color-green'>
                 <label className='take-payment__label'>Direct to Principal:</label>
-                <CurrencyInput value={0} />
+                <CurrencyInput
+                    value={BreakdownPrincipal}
+                    onChange={({ value }) => {
+                        changeAccountTakePayment("BreakdownPrincipal", value as number);
+                    }}
+                />
             </div>
             <div className='take-payment__item color-green'>
                 <label className='take-payment__label'>Contract Payment:</label>
-                <CurrencyInput value={0} />
+                <CurrencyInput
+                    value={BreakdownContractPayment}
+                    onChange={({ value }) => {
+                        changeAccountTakePayment("BreakdownContractPayment", value as number);
+                    }}
+                />
             </div>
         </div>
     );
