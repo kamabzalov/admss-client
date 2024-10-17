87d9819392b211f3f6e6e0283f445280c8728fce
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-10-17 20:39:28 +0300

    Added non-physical person block co-buyer info

--- src/dashboard/contacts/form/general-info/tabs/ofac-check/index.tsx
+++ src/dashboard/contacts/form/general-info/tabs/ofac-check/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { observer } from "mobx-react-lite";
 import { Button } from "primereact/button";
-import { ReactElement } from "react";
+import { ReactElement, useMemo } from "react";
 import "./index.css";
 import { useParams } from "react-router-dom";
 import { checkContactOFAC } from "http/services/contacts-service";
@@ -9,7 +9,7 @@ import { ContactOFAC, OFAC_CHECK_STATUS } from "common/models/contact";
 import { Status } from "common/models/base-response";
 import { useToast } from "dashboard/common/toast";
 import { TOAST_LIFETIME } from "common/settings";
-import { GENERAL_CONTACT_TYPE } from "dashboard/contacts/form/general-info";
+import { BUYER_ID, GENERAL_CONTACT_TYPE } from "dashboard/contacts/form/general-info";
 import { OFACCheckFailedLayout, OFACCheckPassedLayout } from "./ofac-layouts";
 import { useStore } from "store/hooks";
 
@@ -21,7 +21,12 @@ export const ContactsOfacCheck = observer(({ type }: ContactsOfacCheckProps): Re
     const { id } = useParams();
     const toast = useToast();
     const store = useStore().contactStore;
-    const { contactOFAC } = store;
+    const { contactOFAC, contact } = store;
+
+    const isControlDisabled = useMemo(
+        () => type === GENERAL_CONTACT_TYPE.CO_BUYER && contact.type !== BUYER_ID,
+        [type, contact.type]
+    );
 
     const handleOfacCheck = () => {
         checkContactOFAC(id).then((response) => {
@@ -47,6 +52,7 @@ export const ContactsOfacCheck = observer(({ type }: ContactsOfacCheckProps): Re
                     onClick={handleOfacCheck}
                     outlined
                     type='button'
+                    disabled={isControlDisabled}
                 />
             </div>
 
