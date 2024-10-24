0da94d7376ffcd6a1cd5fc25f391afc4918b269e
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-10-24 19:58:55 +0300

    Added page 404

--- src/not-found/index.tsx
+++ src/not-found/index.tsx
@@ -1,3 +1,20 @@
-export default function NotFound() {
-    return <h1>Not found</h1>;
-}
+import { ReactElement } from "react";
+import { Card } from "primereact/card";
+import image404 from "assets/images/404.svg";
+import "./index.css";
+import { Button } from "primereact/button";
+
+export const NotFound = (): ReactElement => {
+    return (
+        <Card className='not-found card'>
+            <h2 className='not-found__warning'>OOPS!</h2>
+            <img className='not-found__image' src={image404} alt='404' />
+            <h3 className='not-found__info'>Page not found</h3>
+            <p className='not-found__message'>The requested URL was not fount on this server</p>
+            <div className='not-found__buttons'>
+                <Button className='not-found__button'>Go back</Button>
+                <Button className='not-found__button'>Contact us</Button>
+            </div>
+        </Card>
+    );
+};
