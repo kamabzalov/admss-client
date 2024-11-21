c278cfeb50f43bd8d4db204f3963ce65744f42d2
Oleksandr Dolinsky <102165823+JSDID@users.noreply.github.com>
2024-11-21 18:46:33 +0200

    Changed disclosure form

--- src/dashboard/inventory/form/vehicle/disclosures/index.tsx
+++ src/dashboard/inventory/form/vehicle/disclosures/index.tsx
@@ -1,12 +1,10 @@
-import { Dropdown } from "primereact/dropdown";
 import { InputTextarea } from "primereact/inputtextarea";
 import { ReactElement } from "react";
 import "./index.css";
-import { Checkbox } from "primereact/checkbox";
 import { observer } from "mobx-react-lite";
 import { useStore } from "store/hooks";
 import { InventoryExtData } from "common/models/inventory";
-import { STATES_LIST } from "common/constants/states";
+import { BorderedCheckbox, StateDropdown } from "dashboard/common/form/inputs";
 
 export const VehicleDisclosures = observer((): ReactElement => {
     const store = useStore().inventoryStore;
@@ -32,120 +30,86 @@ export const VehicleDisclosures = observer((): ReactElement => {
 
     return (
         <div className='grid vehicle-disclosures row-gap-2'>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        className='mt-1'
-                        inputId='disclosures-excess'
-                        onChange={() => handleChange("damODOMInExcess", damODOMInExcess)}
-                        checked={!!damODOMInExcess}
-                    />
-                    <label htmlFor='disclosures-excess' className='ml-2'>
-                        Odometer reflects the milage in EXCESS of its limits
-                    </label>
-                </div>
+            <div className='flex col-12 py-3'>
+                <h3 className='text-line__title text-line__title--red m-0 pr-3'>
+                    Odometer discrepancy
+                </h3>
+                <hr className='text-line__line flex-1' />
             </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        className='mt-1'
-                        inputId='disclosures-discrepancy'
-                        onChange={() => handleChange("damODOMNotActual", damODOMNotActual)}
-                        checked={!!damODOMNotActual}
-                    />
-                    <label htmlFor='disclosures-discrepancy' className='ml-2'>
-                        Odometer is NOT the actual mileage - DISCREPANCY
-                    </label>
-                </div>
+            <div className='col-6'>
+                <BorderedCheckbox
+                    className='mt-1'
+                    inputId='disclosures-excess'
+                    onChange={() => handleChange("damODOMInExcess", damODOMInExcess)}
+                    checked={!!damODOMInExcess}
+                    name='Odometer reflects the milage in EXCESS of its limits'
+                />
             </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        inputId='disclosures-salvage'
-                        className='mt-1'
-                        name='disclosures-salvage'
-                        onChange={() => handleChange("damSalvage", damSalvage)}
-                        checked={!!damSalvage}
-                    />
-                    <label htmlFor='disclosures-salvage' className='ml-2'>
-                        Vehicle is a Salvage Vehicle
-                    </label>
-                </div>
+            <div className='col-6'>
+                <BorderedCheckbox
+                    className='mt-1'
+                    inputId='disclosures-discrepancy'
+                    onChange={() => handleChange("damODOMNotActual", damODOMNotActual)}
+                    checked={!!damODOMNotActual}
+                    name='Odometer is NOT the actual mileage - DISCREPANCY'
+                />
+            </div>
+
+            <div className='flex col-12 py-3'>
+                <h3 className='text-line__title text-line__title--red m-0 pr-3'>
+                    Damage discrepancy
+                </h3>
+                <hr className='text-line__line flex-1' />
+            </div>
+
+            <div className='col-6'>
+                <BorderedCheckbox
+                    inputId='disclosures-salvage'
+                    className='mt-1'
+                    onChange={() => handleChange("damSalvage", damSalvage)}
+                    checked={!!damSalvage}
+                    name='Vehicle is a Salvage Vehicle'
+                />
             </div>
 
             <div className='col-3'>
-                <span className='p-float-label'>
-                    <Dropdown
-                        optionLabel='name'
-                        optionValue='id'
-                        value={damSalvageState}
-                        filter
-                        onChange={({ value }) =>
-                            changeInventoryExtData({ key: "damSalvageState", value })
-                        }
-                        options={STATES_LIST}
-                        className='w-full vehicle-disclosures__dropdown'
-                    />
-                    <label className='float-label'>State</label>
-                </span>
+                <StateDropdown
+                    name='State'
+                    showClear={!!damSalvageState}
+                    value={damSalvageState}
+                    onChange={({ value }) =>
+                        changeInventoryExtData({ key: "damSalvageState", value })
+                    }
+                />
             </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        inputId='disclosures-flood'
-                        className='mt-1'
-                        name='disclosures-flood'
-                        onChange={() => handleChange("damFlood", damFlood)}
-                        checked={!!damFlood}
-                    />
-                    <label htmlFor='disclosures-flood' className='ml-2'>
-                        Vehicle is a Flood Vehicle
-                    </label>
-                </div>
+            <div className='col-6 mr-2'>
+                <BorderedCheckbox
+                    inputId='disclosures-flood'
+                    className='mt-1'
+                    onChange={() => handleChange("damFlood", damFlood)}
+                    checked={!!damFlood}
+                    name='Vehicle is a Flood Vehicle'
+                />
             </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        inputId='disclosures-damage'
-                        className='mt-1'
-                        name='disclosures-damage'
-                        onChange={() => handleChange("dam25", dam25)}
-                        checked={!!dam25}
-                    />
-                    <label htmlFor='disclosures-damage' className='ml-2'>
-                        Vehicle suffered damage of at least 25%
-                    </label>
-                </div>
-            </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        inputId='disclosures-recovered'
-                        className='mt-1'
-                        name='disclosures-recovered'
-                        onChange={() => handleChange("damTheft", damTheft)}
-                        checked={!!damTheft}
-                    />
-                    <label htmlFor='disclosures-recovered' className='ml-2'>
-                        Vehicle is a Recovered Theft Vehicle
-                    </label>
-                </div>
-            </div>
-            <div className='col-3'>
-                <div className='vehicle-disclosures__checkbox flex'>
-                    <Checkbox
-                        inputId='disclosures-reconstructed'
-                        className='mt-1'
-                        name='disclosures-reconstructed'
-                        onChange={() => handleChange("damReconstructed", damReconstructed)}
-                        checked={!!damReconstructed}
-                    />
-                    <label htmlFor='disclosures-reconstructed' className='ml-2'>
-                        Vehicle has been Reconstructed
-                    </label>
-                </div>
+            <div className='col-6 mr-2'>
+                <BorderedCheckbox
+                    inputId='disclosures-reconstructed'
+                    className='mt-1'
+                    onChange={() => handleChange("damReconstructed", damReconstructed)}
+                    checked={!!damReconstructed}
+                    name='Vehicle has been Reconstructed'
+                />
             </div>
 
+            <div className='col-6'>
+                <BorderedCheckbox
+                    inputId='disclosures-damage'
+                    className='mt-1'
+                    onChange={() => handleChange("dam25", dam25)}
+                    checked={!!dam25}
+                    name='Vehicle suffered damage of at least 25%'
+                />
+            </div>
             <div className='col-6'>
                 <span className='p-float-label'>
                     <InputTextarea
@@ -158,6 +122,17 @@ export const VehicleDisclosures = observer((): ReactElement => {
                     <label className='float-label'>Parts Damaged</label>
                 </span>
             </div>
+
+            <div className='col-6'>
+                <BorderedCheckbox
+                    inputId='disclosures-recovered'
+                    className='mt-1'
+                    onChange={() => handleChange("damTheft", damTheft)}
+                    checked={!!damTheft}
+                    name='Vehicle is a Recovered Theft Vehicle'
+                />
+            </div>
+
             <div className='col-6'>
                 <span className='p-float-label'>
                     <InputTextarea
