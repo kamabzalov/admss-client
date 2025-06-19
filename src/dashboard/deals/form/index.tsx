62536f8bdb411c6602fcd12024df332b2b256f11
danhalf <danhalf90@gmail.com>
2025-06-19 17:14:47 +0300

    FEAT: add beforeunload event listener to prevent form loss on navigation

--- src/dashboard/deals/form/index.tsx
+++ src/dashboard/deals/form/index.tsx
@@ -318,6 +318,18 @@ export const DealsForm = observer(() => {
         }
     }, [stepActiveIndex, printActiveIndex, accordionSteps]);
 
+    useEffect(() => {
+        const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+            if (isFormChanged) {
+                event.preventDefault();
+            }
+        };
+        window.addEventListener("beforeunload", handleBeforeUnload);
+        return () => {
+            window.removeEventListener("beforeunload", handleBeforeUnload);
+        };
+    }, [isFormChanged]);
+
     const handleActivePrintForms = () => {
         navigate(getUrl(printActiveIndex));
         setStepActiveIndex(printActiveIndex);
