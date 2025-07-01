0d66960081f23c438a589fd0cf881e97ea75c99d
Kamil Abzalov <49127222+kamabzalov@users.noreply.github.com>
2025-07-01 17:55:40 +0300

    Revert "#1046: remove test task functionality & keep error handling"

--- src/http/routes/ErrorBoundary.tsx
+++ src/http/routes/ErrorBoundary.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "not-found";
 import { useToast } from "dashboard/common/toast";
 import { AxiosError } from "axios";
 import { TOAST_LIFETIME } from "common/settings";
+import { ServiceUpdate } from "services/service-update";
 
 export const ErrorBoundary = (): ReactElement | null => {
     const error = useRouteError() as AxiosError;
@@ -11,6 +12,8 @@ export const ErrorBoundary = (): ReactElement | null => {
 
     if (error?.status === 404 || error?.response?.status === 404) {
         return <NotFound />;
+    } else if (error?.status === 500 || error?.response?.status === 500) {
+        return <ServiceUpdate />;
     }
 
     toast.current?.show({
