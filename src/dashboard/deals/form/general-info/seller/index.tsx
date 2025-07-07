6b6ce204c40bd549a24f4afc59f467221678d01d
danhalf <danhel@rocketmail.com>
2025-07-07 19:20:17 +0300

    REF: change salesperson handling in DealGeneralSeller

--- src/dashboard/deals/form/general-info/seller/index.tsx
+++ src/dashboard/deals/form/general-info/seller/index.tsx
@@ -1,92 +1,71 @@
 import { observer } from "mobx-react-lite";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import "./index.css";
 import { BorderedCheckbox } from "dashboard/common/form/inputs";
 import { useStore } from "store/hooks";
 import { SalespersonsList } from "common/models/contact";
-import { Deal } from "common/models/deals";
-import { SalespersonSearch } from "dashboard/contacts/common/salesperson-search";
+import { ComboBox } from "dashboard/common/form/dropdown";
+import { getContactsSalesmanList } from "http/services/contacts-service";
 
 enum DealGeneralSellerKeys {
     FIRST_SALES_PERSON_ID = "salesperson1uid",
-    FIRST_SALES_PERSON_NAME = "salesperson1name",
     SECOND_SALES_PERSON_ID = "salesperson2uid",
-    SECOND_SALES_PERSON_NAME = "salesperson2name",
     DIFFERENT_SELLER = "differentSeller",
     DIFFERENT_SELLER_ID = "differentSellerUID",
-    DIFFERENT_SELLER_NAME = "differentSellerInfo",
 }
 
 export const DealGeneralSeller = observer((): ReactElement => {
     const store = useStore().dealStore;
+    const userStore = useStore().userStore;
+    const { authUser } = userStore;
+    const [salespersonList, setSalespersonList] = useState<SalespersonsList[]>([]);
+
     const {
-        deal: { salesperson1name, salesperson2name, differentSeller, differentSellerInfo },
+        deal: { salesperson1uid, salesperson2uid, differentSeller, differentSellerUID },
         changeDeal,
     } = store;
 
-    const handleGetFullInfo = (contact: SalespersonsList, key: keyof Deal) => {
-        changeDeal({
-            key,
-            value: contact.useruid,
-        });
-        switch (key) {
-            case DealGeneralSellerKeys.FIRST_SALES_PERSON_ID:
-                changeDeal({
-                    key: DealGeneralSellerKeys.FIRST_SALES_PERSON_NAME,
-                    value: contact.username,
-                });
-                break;
-            case DealGeneralSellerKeys.SECOND_SALES_PERSON_ID:
-                changeDeal({
-                    key: DealGeneralSellerKeys.SECOND_SALES_PERSON_NAME,
-                    value: contact.username,
-                });
-                break;
-            case DealGeneralSellerKeys.DIFFERENT_SELLER_ID:
-                changeDeal({
-                    key: DealGeneralSellerKeys.DIFFERENT_SELLER_NAME,
-                    value: contact.username,
-                });
-                break;
+    const handleGetSalespersonList = async () => {
+        const response = await getContactsSalesmanList(authUser!.useruid);
+        if (response && Array.isArray(response)) {
+            setSalespersonList(response);
         }
     };
+
+    useEffect(() => {
+        handleGetSalespersonList();
+    }, [authUser]);
 
     return (
         <div className='grid deal-general-seller row-gap-2'>
             <div className='col-6'>
-                <SalespersonSearch
-                    value={salesperson1name}
+                <ComboBox
+                    options={salespersonList}
+                    optionLabel='username'
+                    optionValue='useruid'
+                    value={salesperson1uid}
                     onChange={(event) => {
                         changeDeal({
                             key: DealGeneralSellerKeys.FIRST_SALES_PERSON_ID,
                             value: event.target.value,
                         });
                     }}
-                    onChangeGetFullInfo={(value) => {
-                        handleGetFullInfo(value, DealGeneralSellerKeys.FIRST_SALES_PERSON_ID);
-                    }}
-                    getFullInfo={(contact) => {
-                        handleGetFullInfo(contact, DealGeneralSellerKeys.FIRST_SALES_PERSON_ID);
-                    }}
-                    name='Salesman 1'
+                    label='Salesman 1'
                 />
             </div>
             <div className='col-6'>
-                <SalespersonSearch
-                    value={salesperson2name}
+                <ComboBox
+                    options={salespersonList}
+                    optionLabel='username'
+                    optionValue='useruid'
+                    value={salesperson2uid}
                     onChange={(event) => {
                         changeDeal({
                             key: DealGeneralSellerKeys.SECOND_SALES_PERSON_ID,
                             value: event.target.value,
                         });
                     }}
-                    onChangeGetFullInfo={(value) => {
-                        handleGetFullInfo(value, DealGeneralSellerKeys.SECOND_SALES_PERSON_ID);
-                    }}
-                    getFullInfo={(contact) => {
-                        handleGetFullInfo(contact, DealGeneralSellerKeys.SECOND_SALES_PERSON_ID);
-                    }}
-                    name='Salesman 2'
+                    label='Salesman 2'
                 />
             </div>
 
@@ -106,15 +85,18 @@ export const DealGeneralSeller = observer((): ReactElement => {
             </div>
             {!!differentSeller && (
                 <div className='col-6'>
-                    <SalespersonSearch
-                        name='Seller'
-                        value={differentSellerInfo}
-                        onChangeGetFullInfo={(value) => {
-                            handleGetFullInfo(value, DealGeneralSellerKeys.DIFFERENT_SELLER_ID);
+                    <ComboBox
+                        options={salespersonList}
+                        optionLabel='username'
+                        optionValue='useruid'
+                        value={differentSellerUID}
+                        onChange={(event) => {
+                            changeDeal({
+                                key: DealGeneralSellerKeys.DIFFERENT_SELLER_ID,
+                                value: event.target.value,
+                            });
                         }}
-                        getFullInfo={(contact) => {
-                            handleGetFullInfo(contact, DealGeneralSellerKeys.DIFFERENT_SELLER_ID);
-                        }}
+                        label='Seller'
                     />
                 </div>
             )}
