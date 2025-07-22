5855590854d50cf3145e8f3ec1c37b0caacd9c8e
danhalf <danhel@rocketmail.com>
2025-07-22 18:55:01 +0300

    REF: remove "magic words"

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,5 +1,7 @@
+import { EXCLUDED_PAYLOAD_KEYS } from "common/constants/excluded-payload-keys";
+
 export const filterPostPayload = <T>(payload: T, extraExcludedKeys: string[] = []): T => {
-    const defaultExcludedKeys = ["index", "created", "updated", "status"];
+    const defaultExcludedKeys = EXCLUDED_PAYLOAD_KEYS;
     const excludedKeys = [...defaultExcludedKeys, ...extraExcludedKeys];
     return Object.fromEntries(
         Object.entries(payload as Record<string, string | number>).filter(
