f7b42d699533c94fee8e232f7f1fab6609a46590
danhalf <danhel@rocketmail.com>
2025-09-15 09:37:38 +0300

    FIX: update make string formatting to replace spaces with underscores & clear model list after model changing

--- src/dashboard/inventory/form/vehicle/general/index.tsx
+++ src/dashboard/inventory/form/vehicle/general/index.tsx
@@ -35,6 +35,13 @@ const parseMileage = (mileage: string): number => {
     return parseFloat(mileage.replace(/,/g, ""));
 };
 
+const renderedAuditKeys: (keyof Audit)[] = [
+    "JustArrived",
+    "NeedsCleaning",
+    "DataNeedsUpdate",
+    "ReadyForSale",
+];
+
 export const VehicleGeneral = observer((): ReactElement => {
     const store = useStore().inventoryStore;
     const userStore = useStore().userStore;
@@ -146,16 +153,17 @@ export const VehicleGeneral = observer((): ReactElement => {
         }
     }, [currentLocation, locationList, values.locationuid, store]);
 
-    const handleSelectMake = useCallback(() => {
-        const makeSting = inventory.Make.toLowerCase().replaceAll(" ", "");
+    const handleSelectMake = useCallback(async () => {
+        const makeSting = inventory.Make.toLowerCase();
         if (automakesList.some((item) => item.name.toLocaleLowerCase() === makeSting)) {
-            getAutoMakeModelList(makeSting).then((list) => {
-                if (list && Array.isArray(list) && list.length) {
-                    setAutomakesModelList(list);
-                } else {
-                    setAutomakesModelList([]);
-                }
-            });
+            const list = await getAutoMakeModelList(makeSting.replaceAll(" ", "_"));
+            if (list && Array.isArray(list) && list.length) {
+                setAutomakesModelList(list);
+            } else {
+                setAutomakesModelList([]);
+            }
+        } else {
+            setAutomakesModelList([]);
         }
     }, [automakesList, inventory.Make]);
 
@@ -323,13 +331,6 @@ export const VehicleGeneral = observer((): ReactElement => {
             handleGetInventoryGroupFullInfo(vinInfo.GroupClassName || values.GroupClassName);
         }
     };
-
-    const renderedAuditKeys: (keyof Audit)[] = [
-        "JustArrived",
-        "NeedsCleaning",
-        "DataNeedsUpdate",
-        "ReadyForSale",
-    ];
 
     useEffect(() => {
         const activeKey = renderedAuditKeys.find((key) => inventoryAudit[key] === 1) || null;
@@ -541,7 +542,6 @@ export const VehicleGeneral = observer((): ReactElement => {
                         setFieldValue("Model", value);
                         changeInventory({ key: "Model", value });
                     }}
-                    placeholder='Model (required)'
                     className={`vehicle-general__dropdown w-full ${
                         errors.Model ? "p-invalid" : ""
                     }`}
