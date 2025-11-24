12ebd4fead42c0c85ab214845a87d3812fd89d59
danhalf <danhel@rocketmail.com>
2025-11-24 20:12:41 +0200

    FIX: adjust AddTaskDialog menu positioning & add menu max-height

--- src/dashboard/tasks/add-task-dialog/index.tsx
+++ src/dashboard/tasks/add-task-dialog/index.tsx
@@ -308,8 +308,8 @@ export const AddTaskDialog = observer(
                         dropdownIcon='adms-arrow-bottom'
                         model={taskFilterOptions()}
                         className={`task-dialog__status-button status-button ${getStatusClassName()}`}
-                        menuStyle={{ transformOrigin: "bottom", top: "auto" }}
                         appendTo='self'
+                        menuClassName='status-button__menu'
                     />
                     <Button
                         label={`${currentTask ? "Update" : "Save"}`}
