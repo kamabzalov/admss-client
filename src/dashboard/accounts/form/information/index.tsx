6c372458d737f067fdc93ac526d0c9d95dbbc0e6
danhalf <danhel@rocketmail.com>
2026-03-02 20:43:54 +0200

    FIX: remove home account button

--- src/dashboard/accounts/form/information/index.tsx
+++ src/dashboard/accounts/form/information/index.tsx
@@ -124,7 +124,7 @@ export const AccountInformation = observer((): ReactElement => {
                             },
                         ]}
                     />
-                    {accountPermissions.canEditPayments() && (
+                    {accountPermissions.canEditPartialPayments() && (
                         <Button
                             className='account-info__button'
                             onClick={() =>
